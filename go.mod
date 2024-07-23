ea8c6afea9c9edf7875d93d47a3c2d5f1ca17f83
Joe Corall <jjc223@lehigh.edu>
2024-07-23 08:59:17 -0400

    Add JWT auth

--- go.mod
+++ go.mod
@@ -2,4 +2,20 @@ module github.com/lehigh-university-libraries/fabricator
 
 go 1.22.2
 
-require github.com/lehigh-university-libraries/go-islandora v0.0.0-20240709193244-50f8e60d633d
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/lehigh-university-libraries/go-islandora v0.0.0-20240709193244-50f8e60d633d
+	github.com/lestrrat-go/jwx v1.2.29
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+)
--- internal/handlers/check.go
+++ internal/handlers/check.go
@@ -1,16 +1,77 @@
 package handlers
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/lestrrat-go/jwx/jwk"
 )
+
+const googleCertsURL = "https://www.googleapis.com/oauth2/v3/certs"
 
 func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		slog.Error("Authorization header missing")
+		http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+		return
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+		slog.Error("Token not found")
+		http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+		return
+	}
+
+	tokenString := parts[1]
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		kid, ok := token.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("kid not found in token header")
+		}
+
+		ctx := context.Background()
+		jwksSet, err := jwk.Fetch(ctx, googleCertsURL)
+		if err != nil {
+			return nil, fmt.Errorf("unable to fetch JWK set from %s: %v", googleCertsURL, err)
+		}
+		key, ok := jwksSet.LookupKeyID(kid)
+		if !ok {
+			return nil, fmt.Errorf("unable to find key '%s'", kid)
+		}
+
+		var pubkey interface{}
+		if err := key.Raw(&pubkey); err != nil {
+			return nil, fmt.Errorf("failed to get raw key: %v", err)
+		}
+
+		return pubkey, nil
+	})
+
+	if err != nil || !token.Valid {
+		slog.Error("Unable to validate token", "err", err)
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	slog.Info("Token is valid", "claims", claims)
 
 	if r.ContentLength == 0 {
 		http.Error(w, "Request body is empty", http.StatusBadRequest)
@@ -20,7 +81,7 @@ func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var csvData [][]string
-	err := json.NewDecoder(r.Body).Decode(&csvData)
+	err = json.NewDecoder(r.Body).Decode(&csvData)
 	if err != nil {
 		http.Error(w, "Error parsing CSV", http.StatusBadRequest)
 		return
@@ -31,12 +92,18 @@ func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 	}
 
 	header := csvData[0]
-
 	for rowIndex, row := range csvData[1:] {
 		item := make(map[string]string, len(header))
+		emptyRow := true
 		for colIndex, cell := range row {
 			column := header[colIndex]
 			item[column] = cell
+			if cell != "" {
+				emptyRow = false
+			}
+		}
+		if emptyRow {
+			continue
 		}
 		slog.Info("Parsed row", "row", rowIndex, "values", item)
 	}
