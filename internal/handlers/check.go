fd27a4918c0314668b39c170e38ddbdcd35fa347
Joe Corall <jjc223@lehigh.edu>
2024-11-11 13:57:04 -0500

    move auth into own function, adding a secret bypass for easier testing

--- internal/handlers/check.go
+++ internal/handlers/check.go
@@ -26,67 +26,7 @@ func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
-
-	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		slog.Error("Authorization header missing")
-		http.Error(w, "Authorization header missing", http.StatusUnauthorized)
-		return
-	}
-
-	parts := strings.Split(authHeader, " ")
-	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
-		slog.Error("Token not found")
-		http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
-		return
-	}
-
-	tokenString := parts[1]
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-
-		kid, ok := token.Header["kid"].(string)
-		if !ok {
-			return nil, fmt.Errorf("kid not found in token header")
-		}
-
-		ctx := context.Background()
-		jwksSet, err := jwk.Fetch(ctx, googleCertsURL)
-		if err != nil {
-			return nil, fmt.Errorf("unable to fetch JWK set from %s: %v", googleCertsURL, err)
-		}
-		key, ok := jwksSet.LookupKeyID(kid)
-		if !ok {
-			return nil, fmt.Errorf("unable to find key '%s'", kid)
-		}
-
-		var pubkey interface{}
-		if err := key.Raw(&pubkey); err != nil {
-			return nil, fmt.Errorf("failed to get raw key: %v", err)
-		}
-
-		return pubkey, nil
-	})
-
-	if err != nil || !token.Valid {
-		slog.Error("Unable to validate token", "err", err)
-		http.Error(w, "Invalid token", http.StatusUnauthorized)
-		return
-	}
-
-	claims := token.Claims.(jwt.MapClaims)
-	emailVerified, ok := claims["email_verified"].(bool)
-	if !emailVerified || !ok {
-		slog.Error("Unverified email", "err", err)
-		http.Error(w, "Invalid token", http.StatusUnauthorized)
-		return
-	}
-
-	email, ok := claims["email"].(string)
-	if !ok || len(email) < 11 || email[len(email)-11:] != "@lehigh.edu" {
-		http.Error(w, "Error extracting email from token", http.StatusInternalServerError)
+	if !authRequest(w, r) {
 		return
 	}
 
@@ -98,7 +38,7 @@ func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	var csvData [][]string
-	err = json.NewDecoder(r.Body).Decode(&csvData)
+	err := json.NewDecoder(r.Body).Decode(&csvData)
 	if err != nil {
 		slog.Error("Error parsing CSV", "err", err)
 		http.Error(w, "Error parsing CSV", http.StatusBadRequest)
@@ -262,4 +202,76 @@ func fileExists(filename string) bool {
 		return false
 	}
 	return !info.IsDir()
+}
+
+func authRequest(w http.ResponseWriter, r *http.Request) bool {
+	secret := r.Header.Get("X-Secret")
+	if secret != os.Getenv("SHARED_SECRET") {
+		return true
+	}
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		slog.Error("Authorization header missing")
+		http.Error(w, "Authorization header missing", http.StatusUnauthorized)
+		return false
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+		slog.Error("Token not found")
+		http.Error(w, "Invalid Authorization header format", http.StatusUnauthorized)
+		return false
+	}
+
+	tokenString := parts[1]
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		kid, ok := token.Header["kid"].(string)
+		if !ok {
+			return nil, fmt.Errorf("kid not found in token header")
+		}
+
+		ctx := context.Background()
+		jwksSet, err := jwk.Fetch(ctx, googleCertsURL)
+		if err != nil {
+			return nil, fmt.Errorf("unable to fetch JWK set from %s: %v", googleCertsURL, err)
+		}
+		key, ok := jwksSet.LookupKeyID(kid)
+		if !ok {
+			return nil, fmt.Errorf("unable to find key '%s'", kid)
+		}
+
+		var pubkey interface{}
+		if err := key.Raw(&pubkey); err != nil {
+			return nil, fmt.Errorf("failed to get raw key: %v", err)
+		}
+
+		return pubkey, nil
+	})
+
+	if err != nil || !token.Valid {
+		slog.Error("Unable to validate token", "err", err)
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return false
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	emailVerified, ok := claims["email_verified"].(bool)
+	if !emailVerified || !ok {
+		slog.Error("Unverified email", "err", err)
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return false
+	}
+
+	email, ok := claims["email"].(string)
+	if !ok || len(email) < 11 || email[len(email)-11:] != "@lehigh.edu" {
+		http.Error(w, "Error extracting email from token", http.StatusInternalServerError)
+		return false
+	}
+
+	return true
 }
