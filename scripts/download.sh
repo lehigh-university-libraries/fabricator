738bd6b96979fdfc9e99bd54dece500ed4c67ce3
Joe Corall <jjc223@lehigh.edu>
2024-11-12 09:54:39 -0500

    Refactor GitHub action into more scripts (#6)

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,14 +2,20 @@
 
 set -eou pipefail
 
+regex='^(https:\/\/docs\.google\.com\/spreadsheets\/d\/[a-zA-Z0-9_-]+)'
+if [[ "$URL" =~ $regex ]]; then
+  URL="${BASH_REMATCH[1]}"
+else
+  echo "Invalid URL"
+  exit 1
+fi
+
 # extract sheet ID from https://docs.google.com/spreadsheets/d/foo/edit?gid=0#gid=0
 SHEET_ID=$(echo "$URL" | sed -n 's|.*/d/\(.*\)/.*|\1|p')
 
 response=$(curl -s \
     "https://sheets.googleapis.com/v4/spreadsheets/$SHEET_ID/values/$RANGE" \
     -H "Authorization: Bearer $ACCESS_TOKEN")
-
-echo $response > ok.txt
 
 if echo "$response" | jq -e .values >/dev/null; then
   # save as a CSV
@@ -18,5 +24,20 @@ if echo "$response" | jq -e .values >/dev/null; then
   echo "$response" | jq -r '.values | map(map(tostring))' > csv.json
 else
   echo "Failed to fetch data: $(echo "$response" | jq -r '.error.message')"
+  exit 1
+fi
+
+nohup ./fabricator -server=1 &
+echo $! > fabricator_pid.txt
+
+# make sure the sheet passes the check my work
+STATUS=$(curl -v \
+  -w '%{http_code}' \
+  -H "X-Secret: $SHARED_SECRET" \
+  -XPOST \
+  --upload-file csv.json \
+  http://localhost:8080/workbench/check)
+if [ "${STATUS}" -gt 299 ] || [ "${STATUS}" -lt 200 ]; then
+  echo "Check my work failed"
   exit 1
 fi
---
+++ scripts/run-workbench.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+if [ ! -d logs ]; then
+  mkdir -p logs
+fi
+
+if [ ! -d configs ]; then
+  mv ../workbench-configs configs
+fi
+
+mv ../*.csv input_data/
+
+# if we had linked agents with additional metadata
+# run that job first
+if [ -f source.agents.csv ]; then
+  python3 workbench --config configs/terms.yml
+
+  # fail the job if workbench logged any errors
+  grep ERROR logs/agents.log && exit 1 || echo "No errors"
+fi
+
+# run the ingest
+python3 workbench --config configs/create.yml
+
+# fail the job if workbench logged any errors
+grep ERROR logs/items.log && exit 1 || echo "No errors"
