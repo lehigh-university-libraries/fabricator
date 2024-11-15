f8f741c8447eefa64b72086d84b8f33151fca236
Joe Corall <jjc223@lehigh.edu>
2024-11-15 12:30:37 -0500

    refactor download and transform so slack message can just grab the file's values

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -11,26 +11,16 @@ if [ -v URL ]; then
     exit 1
   fi
 
-  # extract sheet ID from https://docs.google.com/spreadsheets/d/foo/edit?gid=0#gid=0
-  SHEET_ID=$(echo "$URL" | sed -n 's|.*/d/\(.*\)|\1|p')
-  TITLE=$(curl -s \
-    -H "Authorization: Bearer $ACCESS_TOKEN" \
-    "https://sheets.googleapis.com/v4/spreadsheets/$SHEET_ID?fields=properties(title)" | jq -r .properties.title
-  )
+  TITLE=$(cat title.txt)
   MESSAGE="${MESSAGE//__TITLE__/$TITLE}"
   MESSAGE="${MESSAGE//__URL__/$URL\/edit\?gid=0#gid=0}"
 
-  LINE_COUNT=$(curl -s \
-    "https://sheets.googleapis.com/v4/spreadsheets/$SHEET_ID/values/$RANGE" \
-    -H "Authorization: Bearer $ACCESS_TOKEN" | jq '.values | length')
-
+  LINE_COUNT=$(jq '.values | length' gsheet.json)
   LINE_COUNT=$(( LINE_COUNT - 1))
   MESSAGE="${MESSAGE//__LINE_COUNT__/$LINE_COUNT}"
-
 fi
 
 escaped_message=$(echo "$MESSAGE" | jq -Rsa .)
-echo $escaped_message
 curl -s -o /dev/null -XPOST "$SLACK_WEBHOOK_URL" -d '{
   "text": '"$escaped_message"'
 }'
