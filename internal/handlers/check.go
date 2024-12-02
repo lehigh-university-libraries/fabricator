a8e1c8c84e793c39cee11b1dcb431a1ef4573841
Joe Corall <jjc223@lehigh.edu>
2024-12-02 11:28:15 -0500

    Do not require resource type

--- internal/handlers/check.go
+++ internal/handlers/check.go
@@ -63,7 +63,6 @@ func CheckMyWork(w http.ResponseWriter, r *http.Request) {
 		"Title",
 		"Object Model",
 		"Full Title",
-		"Resource Type",
 	}
 	uploadIds := map[string]bool{}
 	for rowIndex, row := range csvData[1:] {
